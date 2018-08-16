505902eed132415fef11b4c01a70042a54f48ad7
Orhun Mert Simsek <mert@apps.com.tr>
2018-08-16 09:40:49 +0300

    Initial Commit

---
+++ APLogger/APLogger.h
@@ -0,0 +1,19 @@
+//
+//  APLogger.h
+//  APLogger
+//
+//  Created by Orhun Mert Şimşek on 16.08.2018.
+//  Copyright © 2018 Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APLogger.
+FOUNDATION_EXPORT double APLoggerVersionNumber;
+
+//! Project version string for APLogger.
+FOUNDATION_EXPORT const unsigned char APLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APLogger/PublicHeader.h>
+
+
